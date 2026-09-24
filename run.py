70f981ebdd30fe8bb9fef17366f2a3d73ab4d326
juans <juansotag1@hotmail.com>
2026-09-24 12:49:24 -0500

    fix: ensure uvicorn always binds to 0.0.0.0 in railway

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    host = os.environ.get("HOST", "0.0.0.0") if "PORT" in os.environ else "127.0.0.1"
     port = int(os.environ.get("PORT", 8000))
+    host = "0.0.0.0" if "PORT" in os.environ else "127.0.0.1"
     reload = "PORT" not in os.environ
 
     print(f"Iniciando el servidor del Avatar de Entrenamiento en {host}:{port}...")
